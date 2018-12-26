28f885dc16e8f0fd89dddc26d6165ac89f9640f2
Wahib <wahib1900@gmail.com>
2018-12-26 12:50:45 +0500

    Added the UI along tab and gallery ngx-gallery packages

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,9 +2,12 @@ import { AuthGuard } from './_guards/auth.guard';
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
   // Protecting multiple routes with single route guard
@@ -28,7 +31,8 @@ export const appRoutes: Routes = [
   // Protect muliple routes with multiple guard
   { path: 'home', component: HomeComponent },
   // { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+  { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] , resolve: {users: MemberListResolver}},
+  { path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard] , resolve : {user: MemberDetailResolver}},
   { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
   { path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
   { path: '**' , redirectTo: 'home', pathMatch: 'full'}
