7871f0cf1dc0654905b45b3db244543699edd0fd
Wahib <wahib1900@gmail.com>
2019-01-24 12:59:14 +0500

    Pagination generic class and filter using by age and created and lastActive

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,13 +1,15 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { User } from './../_models/user';
+import { PaginatedResult } from '../_models/pagination';
+import { map } from 'rxjs/operators';
 
 const httpOptions = {
-   headers: new HttpHeaders({
-     'Authorization': 'Bearer ' + localStorage.getItem('token')
-   })
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
 };
 
 @Injectable({
@@ -19,8 +21,34 @@ export class UserService {
   constructor(private httpclient: HttpClient) { }
 
 
-  getUsers(): Observable<User[]> {
-    return this.httpclient.get<User[]>(this.baseUrl + 'users');
+  getUsers(page?, itemsPerPage? , userParams?): Observable<PaginatedResult<User[]>> {
+    
+    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
+
+    let params = new HttpParams();
+
+    if (page != null && itemsPerPage != null) {      
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    if(userParams != null) {
+      params = params.append('minAge', userParams.minAge);
+      params = params.append('maxAge', userParams.maxAge);
+      params = params.append('gender', userParams.gender);
+      params = params.append('orderBy', userParams.orderBy);
+    }
+
+    return this.httpclient.get<User[]>(this.baseUrl + 'users',  { observe: 'response', params })
+      .pipe(
+        map( response =>{
+          paginatedResult.result = response.body;
+          if(response.headers.get('Pagination') !=null){
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+          }          
+          return paginatedResult;
+        })
+      );
   }
 
   getUser(id: any): Observable<User> {
@@ -28,14 +56,14 @@ export class UserService {
   }
 
   updateUser(id: number, user: User) {
-    return this.httpclient.put(this.baseUrl + 'users/' + id , user);
+    return this.httpclient.put(this.baseUrl + 'users/' + id, user);
   }
 
-  setMainPhoto(userId: number , id: number) {
-    return this.httpclient.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain',{});
+  setMainPhoto(userId: number, id: number) {
+    return this.httpclient.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
   }
 
-  deletePhoto(userId: number , id: number) {
+  deletePhoto(userId: number, id: number) {
     return this.httpclient.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
   }
 
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,37 +28,62 @@ namespace DatingApp.API.Data
 
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
-            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p =>p.IsMain);
+            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<Photo> GetPhoto(int id)
         {
             var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
 
-            return photo; 
+            return photo;
         }
 
         public async Task<User> GetUser(int id)
         {
             var users = await _context.Users.
                             Include(p => p.Photos).
-                            FirstOrDefaultAsync(u => u.Id == id);            
+                            FirstOrDefaultAsync(u => u.Id == id);
 
             return users;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var user = await _context.Users.Include(p => p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.Created).AsQueryable();
 
-            return user;
+            users = users.Where(u => u.Id != userParams.UserId);
+
+            users = users.Where(u => u.Gender == userParams.Gender);
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+            }
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
         {
             var result = await _context.SaveChangesAsync() > 0;
             // return await  _context.SaveChangesAsync() > 0;
-            return result ;
+            return result;
         }
     }
 }
