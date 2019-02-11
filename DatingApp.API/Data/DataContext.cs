6e94eb2f5b897d7c5169147909cdc4a8e968352a
Wahib <wahib1900@gmail.com>
2019-02-11 12:08:11 +0500

    Message Entity added Updated Migrations

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -11,10 +11,11 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            // We have created a Primary key 
+            // We have created a Primary key for likes
             builder.Entity<Like>()
                     .HasKey(k => new { k.LikerId, k.LikeeId });
 
@@ -24,14 +25,30 @@ namespace DatingApp.API.Data
                 .HasOne(u => u.Likee)
                 .WithMany(u => u.Likers)
                 .HasForeignKey(u => u.LikeeId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(deleteBehavior:DeleteBehavior.Restrict);
 
             builder.Entity<Like>()
                   .HasOne(u => u.Liker)
                   .WithMany(u => u.Likees)
                   .HasForeignKey(u => u.LikerId)
+                  .OnDelete(deleteBehavior:DeleteBehavior.Restrict);
+
+            // We are creating Primary key for the user Messages
+            builder.Entity<Message>()
+                    .HasKey(m => new { m.SenderId, m.RecipientId });
+ 
+            // I'm creating many to many RelationShip
+           builder.Entity<Message>()
+                  .HasOne(m => m.Sender)
+                  .WithMany(m=> m.MessagesSent)
+                  .HasForeignKey(m =>m.SenderId)
                   .OnDelete(deleteBehavior: DeleteBehavior.Restrict);
 
+           builder.Entity<Message>()
+                  .HasOne(m => m.Recipient)
+                  .WithMany(m =>m.MessagesReceived)
+                  .HasForeignKey(m =>m.RecipientId)
+                  .OnDelete(deleteBehavior:DeleteBehavior.Restrict);    
         }
     }
 }
